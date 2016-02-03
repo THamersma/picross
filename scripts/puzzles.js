63f00a6f25872d1849ffc1afcf1368860d8a95f1
THamersma <thomas_oss@hotmail.com>
2016-02-03 19:51:31 +0100

    alpha v0.1.0
    First public alpha release.

---
+++ scripts/puzzles.js
@@ -0,0 +1,13 @@
+puzzles = [
+{name: "Four corners", x: 10, y: 10, solution: new Array()},
+{name: "Cross", x: 10, y: 10, solution: new Array()},
+{name: "First", x: 10, y: 10, solution: new Array()},
+{name: "Fox", x: 10, y: 10, solution: new Array()},
+{name: "Pong", x: 10, y: 10, solution: new Array()},
+{name: "Cat", x: 10, y: 10, solution: new Array()},
+{name: "Time", x: 10, y: 10, solution: new Array()},
+{name: "Heart", x: 10, y: 10, solution: new Array()},
+{name: "Cocktail", x: 20, y: 20, solution: new Array()},
+{name: "Big fox", x: 20, y: 20, solution: new Array()},
+{name: "Tree", x: 20, y: 20, solution: new Array()}
+];
